20bec9b7e29158da1a48327658b123fb9224b0f6
unknown <drakewang@gmail.com>
2022-09-17 20:25:12 -0700

    Sketch components

--- src/ChessWrapper.js
+++ src/ChessWrapper.js
@@ -7,8 +7,43 @@ export class ChessWrapper extends React.Component {
 	render() {
 		return (
 			<>
-				<div style={{ border: "solid" }} className="container">
-					<Chessboard id="BasicBoard" />
+				<div style={{ border: "solid" }} className="container bg-secondary">
+					<div className="row">
+
+
+
+
+						{/* Black info */}
+						<div style={{ border: "dotted" }} className="col-sm-8">
+							<span>Black Info Container</span>
+						</div>
+						<div style={{ border: "dotted" }} className="col-sm-4"></div>
+
+
+						{/* Chess Board */}
+						<div style={{ border: "dotted" }} className="col-sm-8">
+							<Chessboard id="BasicBoard" />
+						</div>
+
+						{/* Chess Notations */}
+						<div style={{ border: "dotted" }} className="col-sm-4">
+							<span>Chess Notations</span>
+						</div>
+
+						{/* White info */}
+						<div style={{ border: "dotted" }}  className="col-sm-8">
+							<span>White Info Container</span>
+						</div>
+						<div style={{ border: "dotted" }}  className="col-sm-4"></div>
+
+						<div style={{ border: "dotted" }}  className="col-sm-12">
+							<span>Games</span>
+						</div>
+
+
+
+
+					</div>
 				</div>
 			</>
 		)
